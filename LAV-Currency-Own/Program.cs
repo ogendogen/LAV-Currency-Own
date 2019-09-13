5da527fa2a90ac3cbd6ba7fe7471193d93da0552
ogendogen <kosmos500@interia.pl>
2019-09-13 19:49:21 +0200

    Renaming from GAV to LAV

---
+++ LAV-Currency-Own/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CurrencyExtractor;
+using CurrencyExtractor.Models;
+using CurrencyTransformator;
+using CurrencyTransformator.Models;
+
+namespace GAV_Currency_Own
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to start extracting process...");
+            Console.ReadKey();
+
+            try
+            {
+                Console.WriteLine("Starting extraction...");
+                MediatedSchema mediatedSchema = Extractor.GetOutputFromWeb("EUR");
+                Console.WriteLine("Mediated schema object deserialized");
+
+                Console.WriteLine("Extraction successful");
+                Console.WriteLine("Starting transformation...");
+                FinalOutput finalOutput = Transformator.TransformToOutput(mediatedSchema);
+                string serialized = Serializer.SerializeFinalOutput(finalOutput);
+                Console.WriteLine("Transformation completed");
+
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception occured. Logging details...");
+                Logger.LogException("ErrorLogs.log", e);
+                Console.WriteLine("Logging finished. Press any key to stop application");
+                Console.ReadKey();
+            }
+        }
+    }
+}
